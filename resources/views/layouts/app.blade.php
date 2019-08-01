0f14c08e1a75f35bbe3cfc669771bc5b5e23df0a
unknown <bolivar014@gmail.com>
2019-08-01 14:56:19 -0500

    Correcciones en plantilla ppal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
 
                 <ul class="nav flex-column bg-white mb-0">
                     <li class="nav-item">
-                        <a href="#" class="nav-link text-dark font-italic bg-light" id="btnNav">
+                        <a href="{{ url('/post') }}" class="nav-link text-dark font-italic bg-light" id="btnNav">
                             <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
                             Inicio
                         </a>
@@ -130,37 +130,12 @@
                 <small class="text-uppercase font-weight-bold">Menu</small>
             </button>
 
-            <div class="row text-white">
-                <div class="bg-white p-5 rounded my-12 shadow-sm">
-                    <h2 class="lead font-italic text-gray display-4 text-muted text-center">Bootstrap vertical nav</h2>
-                    <hr>
-                    <p class="lead font-italic text-gray mb-0 text-muted">Build a fixed sidebar using Bootstrap 4 vertical navigation and media objects.</p>
-                    <p class="lead font-italic text-gray text-muted">Snippet by <a href="#" class="text-gray">
-                        <u>Bootstrapious</u></a>
-                    </p>
-                    
-                    <p class="lead font-italic mb-0 text-muted">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                </div>               
-            </div>
-    
-            <div class="separator"></div>
 
             <div class="row text-white">
-                <div class="bg-white p-5 rounded my-12 shadow-sm">
-                    <h2 class="lead font-italic text-gray display-4 text-muted text-center">Bootstrap vertical nav</h2>
-                    <hr>
-                    <p class="lead font-italic text-gray mb-0 text-muted">Build a fixed sidebar using Bootstrap 4 vertical navigation and media objects.</p>
-                    <p class="lead font-italic text-gray text-muted">Snippet by <a href="#" class="text-gray">
-                        <u>Bootstrapious</u></a>
-                    </p>
-                    
-                    <p class="lead font-italic mb-0 text-muted">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <div class="bg-white p-5 rounded my-10 shadow-sm">
+                    @yield('content')               
                 </div>               
             </div>
-    
-
         </div>
         <!-- FIN - Page content holder -->
 
---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+    @section('content')
+        <div class="row text-white">
+            <div class="row text-white">
+                <div class="bg-white p-5 rounded my-12 shadow-sm">
+                    <h2 class="lead font-italic text-gray display-4 text-muted text-center">Bootstrap vertical nav</h2>
+                    <hr>
+                    <p class="lead font-italic text-gray mb-0 text-muted">Build a fixed sidebar using Bootstrap 4 vertical navigation and media objects.</p>
+                    <p class="lead font-italic text-gray text-muted">Snippet by <a href="#" class="text-gray">
+                        <u>Bootstrapious</u></a>
+                    </p>
+                
+                    <p class="lead font-italic mb-0 text-muted">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                </div>               
+            </div>
+        </div>
+
+        <div class="separator"></div>
+
+
+    @endsection
